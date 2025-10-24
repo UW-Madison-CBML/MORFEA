7ae8a073f46c2721b4dd5da857c408390f39a8a9
jlundsgaard <jlundsgaard@ap2001.chtc.wisc.edu>
2025-10-23 23:00:30 -0500

    setup train_model, awaiting data loaded into staged/groups/bhaskar-group

--- train_model.sh
+++ train_model.sh
@@ -4,4 +4,4 @@ source .venv/bin/activate
 
 pip install -r requirements.txt
 
-realpath ./../../../staging/groups/bhaskar_group/
+python train.py $(realpath ./../../../staging/groups/bhaskar_group/ivf-data)
