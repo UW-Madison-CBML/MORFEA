a8f9616a3fb2ded600c04597d3791d2672e9cbc4
JensLundsgaard <jenslundsgaard7@gmail.com>
2025-11-05 09:13:30 -0600

    working on reconstruction visualizer

--- get_img.py
+++ get_img.py
@@ -5,6 +5,9 @@ from dataset_ivf import IVFSequenceDataset
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(DEVICE)
 def main():
     ds = IVFSequenceDataset(os.path.abspath("index.csv"), resize=500, norm="minmax01")
     loader = DataLoader(ds, batch_size=1, shuffle=True, num_workers=4, pin_memory=True)
@@ -13,17 +16,19 @@ def main():
         try:
             model.load_state_dict(torch.load("model_weights.pth",weights_only = True))
         except Exception:
-            torch.save(model.state_dict(), f"model_weights.pth")
+            #torch.save(model.state_dict(), f"model_weights.pth")
             print("model has wrong shape")
             return 
+    model = model.to(DEVICE)
     for vol, x in loader:
-        print(x)
+        model.eval()
+        vol = vol.to(DEVICE)
         recon, _ = model(vol)
-        vol = vol[0,49,0].detach().numpy() * 255
-        recon = recon[0,49,0].detach().numpy() * 255
-        comparison = np.concatenate((vol, recon), axis=0).astype(np.uint8)
-        plt.imsave("grayscale_image.png", comparison, cmap='gray') 
-        break
+        vol = vol[-1,-1,-1].detach().numpy() * 255
+        recon = recon[-1,-1,-1].detach().numpy() * 255
+        comparison = np.concatenate((vol, recon), axis=1).astype(np.uint8)
+        plt.imsave("./imgs/"+str(x)+".png", comparison, cmap='gray') 
+        plt.close()
 
 if __name__ == "__main__":
     main()
---
+++ get_imgs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python -m venv venv
+echo "Hello CHTC from Job $1 running on `whoami`@`hostname`"
+source train_venv/bin/activate
+pip install -r train_requirements.txt
+tar -zxf embryo_dataset.tar.gz
+python build_index.py
+mkdir imgs
+python get_img.py 
+tar -czvf imgs.tar.gz /imgs
+
+rm -r embryo_dataset
+cp *.pth /home/jlundsgaard/ivf
