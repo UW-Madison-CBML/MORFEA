284ff106338d9ce8575f742d88cfed80f9b4b0ca
JensLundsgaard <jenslundsgaard7@gmail.com>
2025-11-07 12:30:46 -0600

    added image reconstruction visualizer

--- train_model.sh
+++ train_model.sh
@@ -7,11 +7,14 @@ cat train_requirements.txt
 pip freeze 
 tar -zxf embryo_dataset.tar.gz
 python build_index.py
-export PYTORCH_ALLOC_CONF="expandable_segments:True"
-export PYTORCH_CUDA_ALLOC_CONF="max_split_size_mb:1024"
+#export PYTORCH_ALLOC_CONF="expandable_segments:True"
+#export PYTORCH_CUDA_ALLOC_CONF="max_split_size_mb:1024"
 
 python train.py 
 
+mkdir imgs
+python get_img.py 
+tar -czvf imgs.tar.gz ./imgs
+
 
 rm -r embryo_dataset
-cp *.pth /home/jlundsgaard/ivf
