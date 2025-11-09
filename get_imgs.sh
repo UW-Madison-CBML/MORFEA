7645b08123460282697b8fd73cb585761b269f69
JensLundsgaard <jenslundsgaard7@gmail.com>
2025-11-08 21:34:20 -0600

    fixed some issues with sh files

--- get_imgs.sh
+++ get_imgs.sh
@@ -5,9 +5,11 @@ source venv/bin/activate
 pip install -r train_requirements.txt
 tar -zxf embryo_dataset.tar.gz
 python build_index.py
+rm -r imgs 
+rm imgs.tar.gz
 mkdir imgs
 python get_img.py 
-tar -czvf imgs.tar.gz /imgs
+tar -czvf imgs.tar.gz imgs
 
 rm -r embryo_dataset
 cp *.pth /home/jlundsgaard/ivf
--- train_model.sh
+++ train_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python -m venv venv
 echo "Hello CHTC from Job $1 running on `whoami`@`hostname`"
-source train_venv/bin/activate
+source venv/bin/activate
 pip install -r train_requirements.txt
 cat train_requirements.txt
 pip freeze 
@@ -11,7 +11,8 @@ python build_index.py
 #export PYTORCH_CUDA_ALLOC_CONF="max_split_size_mb:1024"
 
 python train.py 
-
+rm -r imgs
+rm imgs.tar.gz
 mkdir imgs
 python get_img.py 
 tar -czvf imgs.tar.gz ./imgs
