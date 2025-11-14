eb0565dea071c7ed4689b17c7ed91349e3194d13
JensLundsgaard <jenslundsgaard7@gmail.com>
2025-11-13 20:32:41 -0600

    fixed getimg.py as well

--- get_img.py
+++ get_img.py
@@ -20,14 +20,14 @@ def main():
             print("model has wrong shape")
             return 
     model = model.to(DEVICE)
-    for vol, x in loader:
+    for vol, x, _ in loader:
         model.eval()
         vol = vol.to(DEVICE)
         recon, _ = model(vol)
         vol = vol[-1,-1,-1].cpu().detach().numpy() * 255
         recon = recon[-1,-1,-1].cpu().detach().numpy() * 255
         comparison = np.concatenate((vol, recon), axis=1).astype(np.uint8)
-        plt.imsave("./imgs/"+str(x[0])+".png", comparison, cmap='gray') 
+        plt.imsave("./imgs/"+str(x)+".png", comparison, cmap='gray') 
         plt.close()
 
 if __name__ == "__main__":
