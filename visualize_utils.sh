d9cb819c0dce5aaae9657446e30c8f59600f6bcc
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-01-01 13:19:56 -0600

    getting ablated model visualizations

--- visualize_utils.sh
+++ visualize_utils.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 tar -xvzf embryo_dataset_annotations.tar.gz
+tar -xvf latents.tar.gz
 mkdir grades
-python visualize_utils.py latents.csv --output grades --compare-grades --grades-file embryo_dataset_grades.csv --plot-type grid --coloring phase
-python visualize_utils.py latents.csv --output grades --compare-grades --grades-file embryo_dataset_grades.csv --plot-type grid --coloring velocity
-python visualize_utils.py latents.csv --output grades --compare-grades --grades-file embryo_dataset_grades.csv --plot-type grid --coloring curvature
+
+cat visualize_models.txt | xargs -I {} sh -c '
+python visualize_utils.py "$1" --output grades --compare-grades --grades-file embryo_dataset_grades.csv --plot-type grid --coloring phase && \
+python visualize_utils.py "$1" --output grades --compare-grades --grades-file embryo_dataset_grades.csv --plot-type grid --coloring velocity && \
+python visualize_utils.py "$1" --output grades --compare-grades --grades-file embryo_dataset_grades.csv --plot-type grid --coloring curvature
+' -- {}
 
 tar -cvf grades.tar.gz grades
