1005230fa9041b90830b4d7cdf9e541d29d05220
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-02-01 21:54:03 -0600

    workin on stuff

--- grade_lstm_dataset.py
+++ grade_lstm_dataset.py
@@ -21,23 +21,23 @@ class GradeLSTMDataset(Dataset):
         self.grade = grade
         self.latents_df = latents_df.rename(columns={"cell_id":"embryo_id", "video_name":"embryo_id"})
         self.grades_df = grades_df.rename(columns={"cell_id":"embryo_id", "video_name":"embryo_id"})
-        if(not("embryo_id" in self.sig_df.columns and "embryo_id" in self.grades_df.columns)):
-            print(self.sig_df.head())
+        if(not("embryo_id" in self.latents_df.columns and "embryo_id" in self.grades_df.columns)):
+            print(self.latents_df.head())
             print(self.grades_df.head())
             raise ValueError("no embryo_id column")
-        self.lat_cols = [col for col in self.sig_df.columns if col.startswith("z_")]
+        self.lat_cols = [col for col in self.latents_df.columns if col.startswith("z_")]
         self.df =  self.latents_df.merge(self.grades_df, how="left", left_on="embryo_id", right_on="embryo_id")[["embryo_id", self.grade] + self.lat_cols] if self.keep_na else self.latents_df.merge(self.grades_df, how="left", left_on="embryo_id", right_on="embryo_id")[["embryo_id", self.grade] + self.lat_cols].dropna(subset=[self.grade])
         self.df = self.df.sort_values(["embryo_id", "time_step"])
-        sig_data = self.df[self.lat_cols].values
-        self.mean = sig_data.mean(axis=0)
-        self.std = (sig_data.std(axis=0) + 1e-8).astype(np.float32)  # Avoid division by zero
+        lat_data = self.df[self.lat_cols].values
+        self.mean = lat_data.mean(axis=0)
+        self.std = (lat_data.std(axis=0) + 1e-8).astype(np.float32)  # Avoid division by zero
 
     def __getitem__(self, idx):
         grade_options = ["A", "B", "C", "NA"] if self.keep_na else ["A","B","C"]
         embryo_id = self.df.iloc[idx]["embryo_id"]
         rows = self.df.iloc[:idx][self.df["embryo_id"] == embryo_id]
         lat_seq = rows[[i for i in self.df.columns.tolist() if i[:2] == "z_"]].to_numpy(dtype=np.float32)
-        #lat_seq = (signature - self.mean)
+        lat_seq = (lat_seq - self.mean) / self.std
         grade_index = grade_options.index(rows[-1][self.grade])
         return lat_seq.astype(np.float32), grade_index
 
