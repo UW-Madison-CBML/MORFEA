f5032a83f0734d5232c0111a3178f9ad254ab674
balaji29 <balaji29@ap2001.chtc.wisc.edu>
2026-02-12 20:21:48 -0600

    indentation error

--- ivf/stage_dataset.py
+++ ivf/stage_dataset.py
@@ -7,20 +7,20 @@ from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
  
 def addAnnotations(group_name, group, annotations_dir):
-        annotation_file = os.path.join(annotations_dir, f"{group_name}_phases.csv")
+    annotation_file = os.path.join(annotations_dir, f"{group_name}_phases.csv")
+    df = pd.read_csv(annotation_file, names=['stage_id', 'stage_begin', 'stage_end'])
+    new_column = []
+    new_column += ["pre_phase"] * (df.iloc[0]["stage_begin"] - 1)
+        
 
-        df = pd.read_csv(annotation_file, names=['stage_id', 'stage_begin', 'stage_end'])
-	new_column = []
+    for index, row in df.iterrows():
 
-        new_column += ["pre_phase"] * (df.iloc[0]["stage_begin"] - 1)
-        for index, row in df.iterrows():
+        new_column += [row["stage_id"]] * (row["stage_end"] - row["stage_begin"])
 
-        	new_column += [row["stage_id"]] * (row["stage_end"] - row["stage_begin"])
+    new_column += ["post_phase"] * (len(group) - len(new_column))
+    group["phase"] = new_column
 
-        new_column += ["post_phase"] * (len(group) - len(new_column))
-        group["phase"] = new_column
-
-        return group
+    return group
 
 
 class StageModelDataset(Dataset):
@@ -32,17 +32,16 @@ class StageModelDataset(Dataset):
         """
         Args: pd.read_csv(grades_csv, keep_default_na=False).
         """
-	self.latents_df = latents_df
-        self.annotations_dir = annotations_dir
+        self.latents_df = latents_df
+	self.annotations_dir = annotations_dir
 
-        self.df = self.latents_df.groupby("embryo_id").apply(lambda group:addAnnotations(group.name,group,self.annotations_dir))
+	self.df = self.latents_df.groupby("embryo_id").apply(lambda group:addAnnotations(group.name,group,self.annotations_dir))
 	self.groups = self.df.groupby("embryo_id")
-        self.seqlength = 64
+	self.seqlength = 64
 
 
 	self.phases = ['t2', 't3', 't4', 't5', 't6', 't7', 't8', 't9+', 'tB', 'tEB', 'tHB', 'tM', 'tPB2', 'tPNa', 'tPNf', 'tSB', 'pre phase', 'post phase']
-
-        self.lat_cols = [column for i in self.df.columns if i.startswith("z_")]
+	self.lat_cols = [column for i in self.df.columns if i.startswith("z_")]
         # 1. dim reduce latents_df
         # dir/EMBRYO_ID_annotions.csv
         #   stage_id, stage_begin, stage_end
@@ -52,15 +51,15 @@ class StageModelDataset(Dataset):
         # 3. build self.df
 
     def __getitem__(self, idx):
-        
-       	row = self.df.iloc[idx]
+       
+	row = self.df.iloc[idx]
 
-        group = self.groups.get_group(row["embryo_id"]
+	group = self.groups.get_group(row["embryo_id"]
 
-        seqindex = int(((row["time_step"] - 1) / len(group)) * (len(group) - self.seqlength - 1))
+	seqindex = int(((row["time_step"] - 1) / len(group)) * (len(group) - self.seqlength - 1))
 	
 	seq_df = group.iloc[seqindex : seqindex + self.seqlengths]
-        return seq_df[self.lat_cols].to_numpy(), torch.tensor([self.phases.index(row["phase"]) for _,row in seq_df.iterrows()], dtype = torch.long)
+	return seq_df[self.lat_cols].to_numpy(), torch.tensor([self.phases.index(row["phase"]) for _,row in seq_df.iterrows()], dtype = torch.long)
 
         
         # [EMBRYO1, 0, .... (latent vector),
@@ -73,6 +72,6 @@ class StageModelDataset(Dataset):
         # return velocity, curvature, stage (stage is an long 64 integer)
  
  
-    def __len__(self):
-        return len(self.df)
+     def __len__(self):
+	return len(self.df)
 
