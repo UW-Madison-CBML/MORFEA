e0c05c19398ccf64e77bc9d8c32b9b58f7bdc003
balaji29 <balaji29@ap2001.chtc.wisc.edu>
2026-02-16 20:01:08 -0600

    new stage dataset file

--- ivf/stage_dataset.py
+++ ivf/stage_dataset.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
@@ -10,18 +11,25 @@ def addAnnotations(group_name, group, annotations_dir):
     annotation_file = os.path.join(annotations_dir, f"{group_name}_phases.csv")
     df = pd.read_csv(annotation_file, names=['stage_id', 'stage_begin', 'stage_end'])
     new_column = []
+
     new_column += ["pre_phase"] * (df.iloc[0]["stage_begin"] - 1)
-
-
+    col_len_seq = []
     for index, row in df.iterrows():
+        new_column += [row["stage_id"]] * (row["stage_end"] - row["stage_begin"]+1)
+        col_len_seq.append(len(new_column))
 
-        new_column += [row["stage_id"]] * (row["stage_end"] - row["stage_begin"])
+    
+
 
     new_column += ["post_phase"] * (len(group) - len(new_column))
+    new_column = new_column[:len(group)]
+    
     group["phase"] = new_column
+
+    
     return group
 
-class StageModelDataset(Dataset):
+class StageDataset(Dataset):
     """
     Dataset that loads complete embryo sequences.
     Each sample is one full embryo with all its frames.
@@ -33,13 +41,14 @@ class StageModelDataset(Dataset):
         self.latents_df = latents_df
         self.annotations_dir = annotations_dir
 
-        self.df = self.latents_df.groupby("embryo_id").apply(lambda group:addAnnotations(group.name,group,self.annotations_dir))
+
+        self.df = self.latents_df.groupby("embryo_id", group_keys = False).apply(lambda group:addAnnotations(group.name,group,self.annotations_dir)).reset_index()
         self.groups = self.df.groupby("embryo_id")
         self.seqlength = 64
 
 
-        self.phases = ['t2', 't3', 't4', 't5', 't6', 't7', 't8', 't9+', 'tB', 'tEB', 'tHB', 'tM', 'tPB2', 'tPNa', 'tPNf', 'tSB', 'pre phase', 'post phase']
-        self.lat_cols = [column for i in self.df.columns if i.startswith("z_")]
+        self.phases = ['t2', 't3', 't4', 't5', 't6', 't7', 't8', 't9+', 'tB', 'tEB', 'tHB', 'tM', 'tPB2', 'tPNa', 'tPNf', 'tSB', 'pre_phase', 'post_phase']
+        self.lat_cols = [column for column in self.df.columns if column.startswith("z_")]
         # 1. dim reduce latents_df
         # dir/EMBRYO_ID_annotions.csv
         #   stage_id, stage_begin, stage_end
@@ -56,7 +65,7 @@ class StageModelDataset(Dataset):
 
         seqindex = int(((row["time_step"] - 1) / len(group)) * (len(group) - self.seqlength - 1))
 
-        seq_df = group.iloc[seqindex : seqindex + self.seqlengths]
+        seq_df = group.iloc[seqindex : seqindex + self.seqlength]
         return seq_df[self.lat_cols].to_numpy(), torch.tensor([self.phases.index(row["phase"]) for _,row in seq_df.iterrows()], dtype = torch.long)
 
 
