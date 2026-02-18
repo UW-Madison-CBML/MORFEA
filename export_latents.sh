f2de990b1a2034af9de1c5e06cd5a97da40bd13d
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-02-18 00:05:18 -0600

    working on speeding up tarring

--- export_latents.sh
+++ export_latents.sh
@@ -23,12 +23,12 @@ IFS="_" read -ra ADDR <<< "$1"
 #for i in "${ADDR[@]}"; do
 #done
 python export_latents.py --name control-2026-02-16 
-#python export_latents.py --name noconv-2026-02-16 
-#python export_latents.py --name notemp-2026-02-16 
+python export_latents.py --name noconv-2026-02-16 
+python export_latents.py --name notemp-2026-02-16 
 
 #cat get_latents.txt | xargs -I {} sh -c 'python export_latents.py --name "{}" --limit 50'
 # Combine into one pass, using pigz for speed
-tar -I 'zstd -1 -T0' -cf latents.tar.zst latents/
+tar -I 'gzip -1' -cf latents.tar.gz latents/
 
 # Cleanup
 echo "Cleaning up..."
