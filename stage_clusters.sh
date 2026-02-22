00a13dce203ff4acaf8caa89879e742a877aaf1d
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-02-22 16:16:40 -0600

    working on stuff

---
+++ stage_clusters.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Installing dependencies..."
+pip install safetensors wandb
+pip install scikit-learn 
+pip install scikit-learn-extra
+# Extract dataset
+mkdir stage_clusters
+echo "Extracting dataset..."
+tar -zxf latents.tar.gz
+tar -zxf embryo_dataset_annotations.tar.gz
+# Set HuggingFace token from api_keys.txt
+if [ -f "api_keys.txt" ]; then
+    WB_KEY=$(tail -n 1 api_keys.txt)
+    export WANDB_KEY=$WB_KEY
+    echo "HuggingFace token loaded from api_keys.txt"
+fi
+ls -lh
+
+ 
+python stage_clusters.py --name "$1"
+tar -czvf stage_clusters.tar.gz stage_clusters/
