24655a0cda0307d0abc0d48f1ebd54dcf044db09
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-03-05 12:45:22 -0600

    working on infonce and cebra time

--- train_model.sh
+++ train_model.sh
@@ -62,8 +62,6 @@ WANDB_KEY=$(tail -n 1 api_keys.txt)
 export WANDB_KEY=$WANDB_KEY
 export TORCH_DISTRIBUTED_DEBUG=DETAIL
 export NCCL_DEBUG=INFO
-pip install info-nce-pytorch
-pip install cebra
 tar -zxf embryo_dataset.tar.gz
 
 # Create training configuration file for reproducibility
