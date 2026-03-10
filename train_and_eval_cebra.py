95d38d2873128dfe4f2b41a0bcb3ccd84fee70fb
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-03-09 20:57:21 -0500

    fixing my stupid mistakes

--- train_and_eval_cebra.py
+++ train_and_eval_cebra.py
@@ -2,6 +2,7 @@ import cebra
 import torch
 from cebra import CEBRA
 import matplotlib.pyplot as plt
+matplotlib.use('Agg') 
 import numpy as np
 import pandas as pd
 import os
@@ -59,13 +60,14 @@ def main(model_name):
             cebra_latents.append(traj)
             cebra_labels.append(np.arange(len(traj)).reshape(-1, 1).astype(np.float32))
     cebra_time_model.fit(cebra_latents, cebra_labels)
-    #cebra_time_model.save("cebra_time_model.pt")
+    cebra_time_model.save(f"{model_name}_cebra_time_model.pt")
 
+    torch.cuda.empty_cache()
     
     max_imgs = 16
     with torch.no_grad():
         for grade in ["A", "B", "C"]:
-            grade_ds = IVFEmbryoDataset(full_seq_df[full_seq_df["embryo_id"] == grade], resize=128, norm="minmax01")
+            grade_ds = IVFEmbryoDataset(full_seq_df[full_seq_df["TE"] == grade], resize=128, norm="minmax01")
             grade_loader = DataLoader(
                 grade_ds,
                 batch_size=1,
@@ -74,18 +76,19 @@ def main(model_name):
                 pin_memory=True,
                 drop_last=False 
             )
+            print(len(grade_loader))
             fig, axes = plt.subplots(4, 4, figsize=(20, 20),subplot_kw={'projection': '3d'})
 
             axes = axes.ravel()
-
+            im = None
             for i, embryo_vol in enumerate(grade_loader):
                 if(i >= len(axes)):
                     break
                 embryo_vol = embryo_vol.to(device) 
                 _, z_seq = model(embryo_vol)
                 traj = z_seq.cpu().detach().numpy()[0] # batch size one just use that batch
-                                
-                cebra_embedding = cebra_time_model.transform(traj, session_id=0) # i guess dont batch it?
+                print(f"running cebra {i}")                
+                cebra_embedding = cebra_time_model.transform(traj,session_id=0) # i guess dont batch it?
                 ax = axes[i]
                 im = ax.scatter(cebra_embedding[:,0], cebra_embedding[:,1], cebra_embedding[:,2], c=np.linspace(0,1,cebra_embedding.shape[0]), cmap='viridis')
 
@@ -96,7 +99,8 @@ def main(model_name):
             plt.tight_layout(rect=[0, 0, 0.85, 1])
             fig.subplots_adjust(right=0.85) 
             cbar_ax = fig.add_axes([0.88, 0.15, 0.03, 0.7]) 
-            fig.colorbar(im, cax=cbar_ax, label='Normalized Time')
+            if im is not None:
+                fig.colorbar(im, cax=cbar_ax, label='Normalized Time')
  
             fig.savefig(os.path.join("cebra_plots",f"{grade}.png"))
             plt.close()
