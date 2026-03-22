798f936531e550ea635081a42be57f17c03a1537
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-03-22 16:35:09 -0500

    working on stuff

--- train_and_eval_cebra.sh
+++ train_and_eval_cebra.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-python -m ruff check . --select F821,E9 || exit 1
+#python -m ruff check . --select F821,E9 || exit 1
 mkdir cebra_plots
 
 tar -xf embryo_dataset.tar.gz
 tar -xf embryo_dataset_annotations.tar.gz
 HF_KEY=$(head -n 1 api_keys.txt)
 export HF_TOKEN=$HF_KEY
-
+pip install iisignature
 python train_and_eval_cebra.py "$1"
 
 tar -czvf cebra_plots.tar.gz cebra_plots/
