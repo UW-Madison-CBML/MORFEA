c7d29e11e969dfe71fd12865a4f2b2222969f8a6
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-04-16 13:24:45 -0500

    Working on latent visualizer

--- embryo_latent_viewer.py
+++ embryo_latent_viewer.py
@@ -1,10 +1,32 @@
 import pandas as pd
 
 # run this with & python embryo_latent_viewer.py in VS code. Will take a sec to show up
-# reqs: trame trame-vuetify trame-vtk "pyvista[jupyter]" trame 'pyvista[all]' panel
+
 # i am running this on windows not sure how it will work on other os's
 # you will need to download latents and annotations to this folder, and eventually the cebra embeddings as well
+# reqs: trame trame-vuetify trame-vtk "pyvista[jupyter]" trame 'pyvista[all]' panel:
+# streamlit>=1.30.0
+# pandas
+# numpy
+# pillow
+# pyvista>=0.40.0
+# stpyvista>=0.0.10
+# trame
+# trame-vuetify
+# trame-vtk
+# nest_asyncio
+# scikit-learn
+# umap-learn
+
+import asyncio
 import nest_asyncio
+try:
+    loop = asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+
 nest_asyncio.apply()
 import streamlit as st
 import pyvista as pv
@@ -40,6 +62,9 @@ def get_annotations_col(group, group_name, group_len, annotations_dir):
 @st.cache_data
 def build_df():
     model_name = "control-2026-03-12"
+    if(os.path.exists(os.path.join("cebra_latents", f"{model_name}.csv")) and os.path.exists(os.path.join("cebra_latents", f"{model_name}.npy"))):
+        cebra_df = pd.read_csv(os.path.abspath(f"cebra_latents/{model_name}.csv"))
+        cebra_np = np.load(os.path.abspath(f"cebra_latents/{model_name}.npy"))
 
     lat_df = pd.read_csv(os.path.abspath(f"latents/{model_name}.csv"), keep_default_na=False).rename(columns={"cell_id":"embryo_id"})
     lat_np = np.load(os.path.abspath(f"latents/{model_name}.npy"))
@@ -56,11 +81,11 @@ def build_df():
     pca = PCA(n_components=3)
     pca_trajs = pca.fit_transform(trajectories)
 
-    df = pd.concat([df, pd.DataFrame(pca_trajs, columns=["pca_1", "pca_2", "pca_3"])], axis=1)
+    df = pd.concat([df, pd.DataFrame(pca_trajs, columns=["pca_1", "pca_2", "pca_3"], index=df.index)], axis=1)
     ump = umap.UMAP(n_components=3, n_neighbors=15)
     umap_trajs = ump.fit_transform(trajectories)
 
-    df = pd.concat([df, pd.DataFrame(umap_trajs, columns=["umap_1", "umap_2", "umap_3"])], axis=1)
+    df = pd.concat([df, pd.DataFrame(umap_trajs, columns=["umap_1", "umap_2", "umap_3"], index=df.index)], axis=1)
     df = df.groupby("embryo_id").apply(lambda group: get_annotations_col(group, group.name, len(group), "embryo_dataset_annotations"), include_groups=False).reset_index()
     return df
 df = build_df()
@@ -79,21 +104,28 @@ reduction_method = st.sidebar.selectbox(
     ("PCA", "UMAP") # also need cebra eventually
 )
 grade = st.sidebar.multiselect(
-    "Dimension Reduction Technique",
+    "Grade",
     ("A", "B", "C", "NA") 
 )
 phase = st.sidebar.multiselect(
-    "Dimension Reduction Technique",
+    "Grade",
     ('pre_phase', 'tPB2', 'tPNa', 'tPNf', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9+', 'tM','tSB','tB', 'tEB', 'tHB', 'post_phase') 
+)
+color_by = st.sidebar.selectbox(
+    "Color By",
+    ("Grade", "Phase", ) 
 )
 num_embryos = st.sidebar.number_input("Number of embryos", min_value=1, step=1)
 
 
 
 embryos = random.sample(df["embryo_id"].unique().tolist(),int(num_embryos))
-points = df[(df["TE"].str.contains("|".join(grade), regex=True)) & (df["phase"].str.contains("|".join(phase), regex=True)) & (df["embryo_id"].str.contains("|".join(embryos), regex=True))][["pca_1", "pca_2", "pca_3"] if reduction_method == "PCA" else ["umap_1", "umap_2", "umap_3"]].to_numpy()
+new_df = df[(df["TE"].str.contains("|".join(grade), regex=True)) & (df["phase"].str.contains("|".join(phase), regex=True)) & (df["embryo_id"].str.contains("|".join(embryos), regex=True))]
 
+points = new_df[["pca_1", "pca_2", "pca_3"] if reduction_method == "PCA" else ["umap_1", "umap_2", "umap_3"]].to_numpy()
+time = new_df["timestep"].to_numpy()
 
+points
 cloud = pv.PolyData(points)
 
 
@@ -102,7 +134,8 @@ col1, col2 = st.columns([2, 1])
 
 with col1:
     st.subheader(f"3D Latent Space ({reduction_method})")
-    
+    pv.set_jupyter_backend('static') # Static is safer for initial debugging
+
     
     plotter = pv.Plotter(window_size=[600, 600])
     sphere_source = pv.Sphere(radius=0.05) 
@@ -118,6 +151,7 @@ with col1:
         smooth_shading=True
     )
     
+    
     plotter.background_color = "black"
     
     html_io = plotter.export_html(None) 
@@ -132,13 +166,11 @@ with col2:
     st.subheader("Embryo Inspection")
     
     # Selection slider to simulate "picking" a point since stpyvista is static-interactive
-    selected_idx = st.slider("Select Embryo Index", 0, T-1, 0)
+    selected_idx = st.slider("Select Embryo Index", 0, len(points)-1, 0)
     
     st.write(f"**Frame:** {selected_idx}")
     #st.write(f"**Intensity:** {intensities[selected_idx]:.4f}")
     
-    # Render Image Placeholder
-    # Replace 'dummy_image.png' with your actual image loading logic
-    # e.g., image = Image.open(f"path/to/embryo_{selected_idx}.png")
+    
     st.image("https://placeholder.com", caption=f"Embryo {selected_idx} View")
 
