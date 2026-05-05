6d50dd23b5e987f659c86d277071e229021aee57
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-05-05 14:13:23 -0500

    refactoring cebra plotting

--- visualize_cebra.sh
+++ visualize_cebra.sh
@@ -6,6 +6,6 @@ tar -xf embryo_dataset.tar.gz
 tar -xf embryo_dataset_annotations.tar.gz
 HF_KEY=$(head -n 1 api_keys.txt)
 export HF_TOKEN=$HF_KEY
-python train_and_eval_cebra.py "$1"
+python visualize_cebra.py "$1"
 
 tar -czvf cebra_plots.tar.gz cebra_plots/
