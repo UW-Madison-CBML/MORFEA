1c4cd17b9ed534c4560beaf73c697c0fb9bc7b80
Jens Lundsgaard <jenslundsgaard7@gmail.com>
2026-05-11 16:53:51 +0000

    working on latent stuff

--- latent_stats.py
+++ latent_stats.py
@@ -3,6 +3,7 @@ import numpy as np
 from geometric_features import get_acc, get_vel, calculate_curvatures
 from scipy.spatial import distance_matrix
 import os
+from scipy.stats import ttest_ind
 def add_geo_features(group, latent_cols):
     trajectory = group[latent_cols].to_numpy()
     curv_5 = calculate_curvatures(trajectory, offset=5, how="triangle")
@@ -21,7 +22,7 @@ def add_geo_features(group, latent_cols):
                                 }, index=group.index)
     return pd.concat([group, features_df], axis=1)
 
-PHASES = ['pre_phase', 'tPB2', 'tPNa', 'tPNf', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9+', 'tM','tSB','tB', 'tEB', 'tHB', 'post_phase']
+PHASES = ['pre_phase', 'tPB2', 'tPNa', 'tPNf', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9+', 'tM','tSB','tB', 'tEB', 'post_phase']
 
 def print_next_to(strings): # strings must be of same # of lines
     line_rows = list(zip(*[s.split("\n") for s in strings]))
@@ -31,24 +32,32 @@ def print_next_to(strings): # strings must be of same # of lines
 
 def main(model_name, grade):
 
-    metadata_df = pd.read_csv(os.path.join("latents", f"{model_name}.csv"), keep_default_na=False)
-    latents_np = np.load(os.path.join("latents", f"{model_name}.npy"))
-    latent_cols = [f"z_{i}" for i in range(latents_np.shape[1])]
-    latents_df = pd.DataFrame(latents_np, columns = latent_cols, index=metadata_df.index)
-    df = pd.concat([metadata_df, latents_df], axis=1)
-    df = df.groupby("embryo_id").apply(lambda group:add_geo_features(group,latent_cols), include_groups=False).reset_index()
-    df = df[~(df[grade] == "NA")]
+    df = None
+    if(not os.path.exists("latent_stats.csv")):
+        metadata_df = pd.read_csv(os.path.join("latents", f"{model_name}.csv"), keep_default_na=False)
+        latents_np = np.load(os.path.join("latents", f"{model_name}.npy"))
+        latent_cols = [f"z_{i}" for i in range(latents_np.shape[1])]
+        latents_df = pd.DataFrame(latents_np, columns = latent_cols, index=metadata_df.index)
+        df = pd.concat([metadata_df, latents_df], axis=1)
+        df = df.groupby("embryo_id").apply(lambda group:add_geo_features(group,latent_cols), include_groups=False).reset_index()
+        df = df[~(df[grade] == "NA")]
+        df.to_csv(os.path.join(os.getcwd(), "latent_stats.csv"))
+    else:
+        df = pd.read_csv(os.path.join(os.getcwd(), "latent_stats.csv"))
+
     # get curvature by grade
     #grade_curv_groups = df.groupby(grade)[["curv_5", "curv_10", "curv_20"]]
     #print(grade_curve_groups.mean(),"\n", grade_curv_groups.std())
     grade_phase_groups = df.groupby([grade, "phase"])
     # ----------------------------------------------------------------------------------
-    # get curvature during phase by grade
+    # do some formatting for latex and run t-test
     
-    grade_phase_curv_groups = grade_phase_groups[["curv_5", "curv_10", "curv_20"]]
+    column = "acc"
+    features = ["vel", "acc"]
+    grade_phase_feature_groups = grade_phase_groups[features]
     grade_phase_df = pd.concat([
-            grade_phase_curv_groups.mean().reindex(PHASES, level="phase"), 
-            grade_phase_curv_groups.std().reindex(PHASES, level="phase").rename(mapper=lambda n: n + "_std", axis = 1),
+            grade_phase_feature_groups.mean().reindex(PHASES, level="phase"), 
+            grade_phase_feature_groups.std().reindex(PHASES, level="phase").rename(mapper=lambda n: n + "_std", axis = 1),
             pd.DataFrame(grade_phase_groups[["time_step"]].size().reindex(PHASES, level="phase"), columns=["count"])
         ],axis=1)    
     grade_phase_df = grade_phase_df.reindex(sorted(grade_phase_df.columns), axis=1)
@@ -56,10 +65,18 @@ def main(model_name, grade):
     styled_df = styled_df.format_index(formatter= lambda s:s.replace("_",r"\_"),axis=1) # format column names
     styled_df = styled_df.format_index(formatter= lambda s:s.replace("_",r"\_"),axis=0) # format index names
 
-    print(styled_df.to_latex(hrules=True))
+    #print(styled_df.to_latex(hrules=True))
     print_next_to([g + " \n" + str(grade_phase_df.loc[g]) for g in ["A","B","C"]])
-    # get displacement by phase by grade
-    # get std dev of each coord by grade
+    for phase in PHASES:
+        print(phase)
+        a_df = grade_phase_feature_groups.get_group(("A", phase))
+        a_np = a_df[column].to_numpy()
+        bc_df = pd.concat([grade_phase_feature_groups.get_group(("B", phase)), grade_phase_feature_groups.get_group(("C", phase))], axis=0)
+        bc_np = bc_df[column].to_numpy()
+
+
+
+        print("welch t-test: ", ttest_ind(a_np, bc_np, equal_var=False))
     # TODO: use ground truth time annotations to compare growth to literature
 
 
