5105caab142d2793b7e54f28001e78a173f4e40d
Jens Lundsgaard <jenslundsgaard7@gmail.com>
2026-05-12 23:05:56 +0000

    working on path sig stuff

--- latent_stats.py
+++ latent_stats.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import numpy as np
-from geometric_features import get_acc, get_vel, calculate_curvatures, get_path_sigs
+from geometric_features import get_acc, get_vel, calculate_curvatures, get_path_sigs,get_path_sig
 from scipy.spatial import distance_matrix
 import os
 from scipy.stats import ttest_ind
+import matplotlib.pyplot as plt
+import umap as UMAP
+from sklearn.decomposition import PCA
+from sklearn.decomposition import FastICA 
+cebra_latent_cols =  ["cebra_0","cebra_1", "cebra_2"]
 def add_geo_features(group, latent_cols):
     trajectory = group[latent_cols].to_numpy()
-    cebra_traj = group[["cebra_0","cebra_1", "cebra_2"]].to_numpy()
-    path_sigs, basis = get_path_sigs(cebra_traj, 3, return_signature_names = True)
+    cebra_traj = group[cebra_latent_cols].to_numpy()
+    path_sigs, basis = get_path_sigs(cebra_traj, 3, return_feature_labels = True)
     
     curv_5 = calculate_curvatures(trajectory, offset=5, how="triangle")
     curv_10 = calculate_curvatures(trajectory, offset=10, how="triangle")
@@ -52,7 +57,7 @@ def print_next_to(strings): # strings must be of same # of lines
     print("\n".join([" ".join([line.ljust(padding_len, " ") for line in line_row]) for line_row in line_rows]))
 
 def main(model_name, grade):
-
+    latent_cols = None
     df = None
     if(not os.path.exists("latent_stats.csv")):
         metadata_df = pd.read_csv(os.path.join("latents", f"{model_name}.csv"), keep_default_na=False)
@@ -67,6 +72,7 @@ def main(model_name, grade):
         df.to_csv(os.path.join(os.getcwd(), "latent_stats.csv"))
     else:
         df = pd.read_csv(os.path.join(os.getcwd(), "latent_stats.csv"))
+        latent_cols = [col for col in df.columns if col.startswith("z_")]
 
     # get curvature by grade
     #grade_curv_groups = df.groupby(grade)[["curv_5", "curv_10", "curv_20"]]
@@ -104,7 +110,31 @@ def main(model_name, grade):
         t_test_series.append(pd.Series(t_s, index=PHASES))
     t_tests_df = pd.DataFrame({features[i]:t_test_series[i] for i in range(len(features))})
     t_tests_df.to_csv(os.path.join(os.getcwd(), "t_test.csv"))
+    
+    # turn each graded embryo into a single path signature feature, by picking along tSB to tEB (each path sig is from just those latents)
+    def group_to_path_sig(group):
+        ps_group = pd.DataFrame([get_path_sig(group[cebra_latent_cols],3)], columns=features)
+        ps_group[grade] = group.iloc[0][grade]
+        return ps_group
+    cebra_latents_group_df = df[(df['phase'].str.contains('tSB|tB|tEB', regex=True))][cebra_latent_cols + [grade, "embryo_id"]].groupby("embryo_id").apply(group_to_path_sig, include_groups=False).reset_index()#
 
+    print(cebra_latents_group_df)
+    cebra_latents_group_df.to_csv(os.path.join(os.getcwd(), "path_sigs_by_embryo.csv"))
+    colors = ["#FF0000", "#FFFF00", '#00FF00']
+    grades = ["C", "B", "A"]
+    umap = UMAP.UMAP(n_components=3) 
+    embedding = umap.fit_transform(cebra_latents_group_df[features].to_numpy())
+    #pca = PCA(n_components=3)
+    #embedding = pca.fit_transform(cebra_latents_group_df[features].to_numpy())
+    #ica = FastICA(n_components=3)
+    #embedding = ica.fit_transform(cebra_latents_group_df[features].to_numpy())
+    #embedding = cebra_latents_group_df[["ps_1","ps_2","ps_[2,3]"]].to_numpy()
+    fig, ax = plt.subplots(subplot_kw={'projection':'3d'})
+    ax.scatter(embedding[:, 0], embedding[:,1], embedding[:,2], c=[colors[grades.index(g)] for g in cebra_latents_group_df[grade].to_list()])
+    fig.savefig(os.path.join(os.getcwd(), "path_sigs.jpg"))
+    plt.close(fig)
+    
+    
     # TODO: use ground truth time annotations to compare growth to literature
 
 
