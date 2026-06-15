36d2a17c80221ac238d38387b3510943278b9122
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-06-15 14:04:54 -0500

    working on vit AE

--- training/vit_model.py
+++ training/vit_model.py
@@ -1,6 +1,7 @@
 # class for new ViT models (both with LSTM and 3d attention?)
 import torch
 import torchvision
+import torch.nn.functionl as F
 class ViTLSTMAE(torch.nn.Module):
     def __init__(self, pretrained = False, latent_dim_per_token = 16, use_lstm = True):
         super().__init__()
@@ -23,7 +24,7 @@ class ViTLSTMAE(torch.nn.Module):
     def forward(self, x):
         B,T,C,H,W =  x.shape # B, T, 1, 224,224
         x_flat_seq = x.view(B*T,1,224,224) 
-        transformed = vit_enc(x) # B*T, 197, 768
+        transformed = self.vit_enc(x) # B*T, 197, 768
         embeddings = transformed[:,1:,:] # B*T, 196, 768
         #num_tokens = embedding.shape[1]
         embeddings = F.relu(self.lin1(embeddings)) # B*T,196, 32
@@ -38,7 +39,7 @@ class ViTLSTMAE(torch.nn.Module):
         latents_flat = latents.permute(1,0,2) # transformer decoder needs batch second
         pos_enc = self.positional_embedding(torch.arange(self.num_tokens, device = x.device)[None,:]).repeat(B*T,1) # B*T, self.num_tokens, latent_dim_per_token
         pos_enc = pos_enc.permute(1,0,2) # need batch first
-        x_rec = transformer_dec(pos_enc, latents_flat) # 196, B*T, 16
+        x_rec = self.transformer_dec(pos_enc, latents_flat) # 196, B*T, 16
         x_rec = x_rec.permute(1, 0, 2) 
         x_rec = F.relu(self.lin3(x_rec)).reshape(B*T,14,14,16,16) # B*T, 196, 256
         x_rec = x_rec.permute(0, 1,3,2,4).reshape(B*T, 224, 224)
