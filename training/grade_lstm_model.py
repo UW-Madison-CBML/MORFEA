ed3287506139c112f8acf1a697c176f883564677
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-06-28 13:28:12 -0500

    working on path sigs and convlstm model

--- training/grade_lstm_model.py
+++ training/grade_lstm_model.py
@@ -4,19 +4,19 @@ from torch.nn.utils.rnn import pack_padded_sequence
 class GradeLSTMClassifier(torch.nn.Module):
     def __init__(self, in_size, keep_na=False):
         super().__init__()
-        self.lin1 = torch.nn.Linear(in_size, 32)
-        self.lin2 = torch.nn.Linear(32, 32)
-        self.lin3 = torch.nn.Linear(32, 32)
-        self.lstm = torch.nn.LSTM(32, 32, batch_first=True)
-        self.lin4 = torch.nn.Linear(32, 4 if keep_na else 3)
-        self.dropout = torch.nn.Dropout(0.4)
+        self.lin1 = torch.nn.Linear(in_size, 64)
+        self.lin2 = torch.nn.Linear(64, 64)
+        self.lin3 = torch.nn.Linear(64, 64)
+        self.lstm = torch.nn.LSTM(64, 64, batch_first=True)
+        self.lin4 = torch.nn.Linear(64, 4 if keep_na else 3)
+        self.dropout = torch.nn.Dropout(0.2)
     def forward(self, x, lengths):
         B,T,L = x.shape 
         x = x.view(B*T,L)
         x = F.relu(self.lin1(x))
         x = self.dropout(F.relu(self.lin2(x)))
         x = F.relu(self.lin3(x))
-        x = x.view(B,T,32)
+        x = x.view(B,T,64)
         x_packed = pack_padded_sequence(x, lengths.cpu(), batch_first=True, enforce_sorted=False)
 
         _, (hn, cn) = self.lstm(x_packed)
