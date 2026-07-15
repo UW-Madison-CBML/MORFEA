f0e5e657754d129f6b79cf5c8aa4c0a5a2bea73e
JensLundsgaard <jenslundsgaard7@gmail.com>
2026-07-15 11:29:49 -0500

    working on downstream tasks

--- training/dataset_ivf_embryo.py
+++ training/dataset_ivf_embryo.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset
 from PIL import Image, ImageFile
 from torchvision.transforms import v2
 from torchvision import tv_tensors
+from torchvision.tv_tensors import Video
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def read_gray(path, resize, crop):
@@ -31,16 +32,20 @@ def normalize_video(vol, norm):
     return vol
 
 class IVFEmbryoDataset(Dataset):
-    def __init__(self, df, crop=45, resize=128, norm="minmax01", return_embryo_id=False):
+    def __init__(self, df, crop=45, resize=128, norm="minmax01", return_embryo_id=False, return_augment=False):
         self.crop = crop
         self.df = df
         self.resize = resize
         self.norm = norm
         self.augment = v2.Compose([
-            v2.RandomHorizontalFlip(p=0.5),
-            v2.RandomHorizontalFlip(p=0.5),
+            v2.RandomHorizontalFlip(p=0.5), 
+            v2.RandomVerticalFlip(p=0.5),
+            v2.RandomCrop(size=500-(2*self.crop)),
+            v2.Resize(size=self.resize)
         ])
+
         self.return_embryo_id = return_embryo_id
+        self.return_augment = return_augment
 
     
     def __getitem__(self, idx):
@@ -53,17 +58,24 @@ class IVFEmbryoDataset(Dataset):
         embryo_paths = row["embryo_paths"].split("|")
 
 
-        embryo_frames = [read_gray(p, self.resize, self.crop) for p in embryo_paths]
-        embryo_vol = np.stack(embryo_frames, axis=0)  # (T, H, W)
-
-        embryo_vol = normalize_video(embryo_vol, self.norm)
-
-        embryo_vol = embryo_vol[:, None, :, :]
-        if self.return_embryo_id:
-            return torch.from_numpy(embryo_vol), row["embryo_id"]
-            
+        embryo_frames1 = [read_gray(p, self.resize, self.crop) for p in embryo_paths]
+        embryo_frames2 = [read_gray(p, None, 0) for p in embryo_paths]
+        embryo_vol1 = np.stack(embryo_frames1, axis=0)  
+        embryo_vol2 = np.stack(embryo_frames2, axis=0)  
+        embryo_vol1 = normalize_video(embryo_vol1, self.norm)
+        embryo_vol2 = normalize_video(embryo_vol2, self.norm)
+        embryo_vol1 = embryo_vol1[:,None, :, :] 
+        embryo_vol2 = embryo_vol2[:,None, :, :] 
+        torch_vol1 = torch.from_numpy(embryo_vol1) 
+        torch_vol2 = torch.from_numpy(embryo_vol2) 
+        if self.return_embryo_id and self.return_augment:
+            return torch_vol1, self.augment(Video(torch_vol2)), row["embryo_id"]
+        elif self.return_augment:
+            return torch_vol1, self.augment(Video(torch_vol2))
+        elif self.return_embryo_id:
+            return torch_vol1, row["embryo_id"]
         else:
-            return torch.from_numpy(embryo_vol)
+            return torch_vol1
 
     def __len__(self):
         return len(self.df)
--- training/train_ps_grade_classifier.py
+++ training/train_ps_grade_classifier.py
@@ -1,11 +1,11 @@
 import torch
 from torch.utils.data import Dataloader
-
+import wandb
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
-from ps_stage_dataset import PathSigStageDataset
+from ps_grade_dataset import PathSigGradeDataset
 import torch.nn.functional as F
 import os
 from sklearn.decomposition import PCA
@@ -14,17 +14,30 @@ from stats_utils import prfcm, disp_cm
 
 class MLP(torch.nn.Module):
     def __init__(self, in_size):
-        self.lin1 = torch.nn.Linear(in_size, 128)
+        self.stage_embedding = torch.nn.Embedding(len(PathSigGradeDataset.PHASES), 3)
+        self.lin1 = torch.nn.Linear(in_size + 3, 128)
         self.lin2 = torch.nn.Linear(128, 128)
         self.lin3 = torch.nn.Linear(128, 128)
-        self.lin4 = torch.nn.Linear(128, len(PathSigStageDataset.PHASES))
+        self.lin4 = torch.nn.Linear(128, len(PathSigGradeDataset.GRADES))
         self.dropout = torch.nn.Dropout(0.3)
-    def forward(self, x):
+    def forward(self, x, stage_classes):
+        x = torch.cat([x, self.stage_embedding(stage_classes)], dim=1)
         x = F.relu(self.lin1(x))
         x = F.relu(self.lin2(x))
         x = self.dropout(x)
         x = self.lin4(F.relu(self.lin3(x)))
         return x
+
+# agg function for stage level confusion mats
+def cm_agg_plot(group):
+    gt = torch.from_numpy(group["gt"].to_numpy())
+    pred = torch.from_numpy(group["pred"].to_numpy())
+    _, cm = prfcm(gt, pred, 3)
+    fig, ax = plt.subplots(figsize=(8,6))
+    disp_cm(cm,PathSigGradeDataset.GRADES, fig, ax)
+    img = wandb.Image(fig)
+    plt.close(fig)
+    return img
 
 def main(model_name):
     # hyperparameters 
@@ -52,6 +65,8 @@ def main(model_name):
 
     pca_df = pd.DataFrame(PCA(n_components=pca_dim).fit_transform(StandardScaler().fit_transform(latents)), columns = [f"pca_{i}" for i in range(pca_dim)], index=metadata_df.index)
     df = pd.concat([metadata_df, pca_df], axis=1)
+    # drop na's 
+    df = df[df['TE'].isin(["A", "B", "C"])]
     
     # shuffle df
     df = df.sample(frac=1, replace=False)
@@ -80,24 +95,37 @@ def main(model_name):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     crit = torch.nn.CrossEntropyLoss()
     for epoch in range(epochs):
-        for features, targets in loader:
+        for features, stage_classes, targets in loader:
             optimizer.zero_grad()
             features = features.to(DEVICE)
             targets = targets.to(DEVICE)
-            logits = model(features)  
+            stage_classes = stage_classes.to(DEVICE)
+            logits = model(features, stage_classes)  
             loss = crit(logits, targets)
             loss.backward()
             optimizer.step()
             run.log({"loss":loss.item()})
-            
+        dfs = []    
         with torch.no_grad():
-            for features, targets in val_loader:
-                run.log({"loss":loss.item()})
+            for features, stage_classes, targets in val_loader:
                 features = features.to(DEVICE)
                 targets = targets.to(DEVICE)
-                logits = model(features)  
+                stage_classes = stage_classes.to(DEVICE)
+                logits = model(features, stage_classes)  
                 loss = crit(logits, targets)
                 run.log({"val_loss":loss.item()})
+                pred = logits.cpu().argmax(dim=1).numpy()
+                gt = targets.cpu().numpy()
+                
+                stage_classes = [PathSigGradeDataset.PHASES.index(s) for s in stage_classes.cpu().numpy()]
+                dfs.append(pd.DataFrame({"pred": pred, "gt":gt, "stage_class":stage_classes}))
+        image_dict = {}
+        results_df = pd.concat(dfs, axis=0, ignore_index=True)
+        image_dict["all_stage_cm"] = cm_agg_plot(results_df)
+        images = results_df.groupby("stage_class").apply(cm_agg_plot).reset_index()
+        for stage, img in images.items()
+            image_dict[f"{stage}_cm"] = img
+        run.log(image_dict)
             
 
             
